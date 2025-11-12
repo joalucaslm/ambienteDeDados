c56abe55ef968bd4d6fb629eb2d5cf3a5744fd8a
João Lucas Lima Maia <160551361+joalucaslm@users.noreply.github.com>
2025-11-12 18:24:15 -0300

    feat: base code

---
+++ java/api/src/test/java/unifor/ambientededados/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package unifor.ambientededados.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

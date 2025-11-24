10f9107ac29b1d8676be799e5ac3144023308402
João Lucas Lima Maia <160551361+joalucaslm@users.noreply.github.com>
2025-11-24 09:14:33 -0300

    feat: create DTO, Repository and Service

---
+++ java/api/src/main/java/com/ambientedados/api/service/UserService.java
@@ -0,0 +1,36 @@
+package com.ambientedados.api.service;
+
+import com.ambientedados.api.controller.CreateUserDto;
+import com.ambientedados.api.entity.User;
+import com.ambientedados.api.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto) {
+        // AQUI É ONDE ACONTECE A INJEÇÃO DE DEPENDÊNCIA
+
+        // DTO -> ENTITY
+        var entity = new User(
+                UUID.randomUUID(),
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                Instant.now(),
+                null);
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserId();
+    }
+}
